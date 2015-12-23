d11632cf2eccede42ad63c6aa3d1ef73226c903a
Kostiantyn Stepaniuk <kostya.stepanyuk@gmail.com>
2015-12-23 21:01:42 +0100

    Add tests for PutConn function

--- db_test.go
+++ db_test.go
@@ -3,6 +3,7 @@ package mysqldriver
 import (
 	"testing"
 
+	"github.com/pubnative/mysqlproto-go"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -10,7 +11,7 @@ func TestDBGetConnSuccessfullyEstablishConnection(t *testing.T) {
 	db := NewDB("root@tcp(127.0.0.1:3306)/test", 1)
 	conn, err := db.GetConn()
 	assert.Nil(t, err)
-	assert.True(t, conn.conn.CapabilityFlags>uint32(0))
+	assert.True(t, conn.conn.CapabilityFlags > uint32(0))
 }
 
 func TestDBGetConnReturnsConnectionFromThePool(t *testing.T) {
@@ -32,6 +33,38 @@ func TestDBGetConnReturnsErrorWhenDBIsClosed(t *testing.T) {
 	db.Close()
 	_, err := db.GetConn()
 	assert.Equal(t, err, ErrClosedDB)
+}
+
+func TestDBPutConnAddsConnectionToThePool(t *testing.T) {
+	db := NewDB("root@tcp(127.0.0.1:3306)/test", 2)
+	assert.Len(t, db.conns, 0)
+	conn, _ := db.GetConn()
+	assert.Nil(t, db.PutConn(conn))
+	assert.Len(t, db.conns, 1)
+}
+
+func TestDBPutConnAddsUpTpPoolSize(t *testing.T) {
+	db := NewDB("root@tcp(127.0.0.1:3306)/test", 2)
+	conn1, _ := db.GetConn()
+	conn2, _ := db.GetConn()
+	conn3, _ := db.GetConn()
+	assert.Len(t, db.conns, 0)
+	assert.Nil(t, db.PutConn(conn1))
+	assert.Len(t, db.conns, 1)
+	assert.Nil(t, db.PutConn(conn2))
+	assert.Len(t, db.conns, 2)
+	assert.Nil(t, db.PutConn(conn3))
+	assert.Len(t, db.conns, 2)
+}
+
+func TestDBPutConnClosesConnectionWhenDBIsClosed(t *testing.T) {
+	db := NewDB("root@tcp(127.0.0.1:3306)/test", 2)
+	db.Close()
+
+	s := &stream{}
+	conn := Conn{mysqlproto.Conn{mysqlproto.NewStream(s), 0}}
+	db.PutConn(conn)
+	assert.True(t, s.closed)
 }
 
 func TestParseDataSourceFull(t *testing.T) {
@@ -63,3 +96,9 @@ func TestParseDataSourceWithoutDatabase(t *testing.T) {
 	assert.Equal(t, addr, "127.0.0.1:3306")
 	assert.Equal(t, dbname, "")
 }
+
+type stream struct{ closed bool }
+
+func (s *stream) Write([]byte) (int, error) { return 0, nil }
+func (s *stream) Read([]byte) (int, error)  { return 0, nil }
+func (s *stream) Close() error              { s.closed = true; return nil }
