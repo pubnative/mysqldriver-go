b374f717d82516f5cfd6e631ab2245f101bb93ee
Kostiantyn Stepaniuk <kostya.stepanyuk@gmail.com>
2015-12-23 19:57:38 +0100

    Add tests for GetConn function

--- db_test.go
+++ db_test.go
@@ -6,6 +6,34 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestDBGetConnSuccessfullyEstablishConnection(t *testing.T) {
+	db := NewDB("root@tcp(127.0.0.1:3306)/test", 1)
+	conn, err := db.GetConn()
+	assert.Nil(t, err)
+	assert.True(t, conn.conn.CapabilityFlags>uint32(0))
+}
+
+func TestDBGetConnReturnsConnectionFromThePool(t *testing.T) {
+	db := NewDB("root@tcp(127.0.0.1:3306)/test", 2)
+	conn1, _ := db.GetConn()
+	conn2, _ := db.GetConn()
+	db.PutConn(conn1)
+	db.PutConn(conn2)
+
+	assert.Len(t, db.conns, 2)
+	db.GetConn()
+	assert.Len(t, db.conns, 1)
+	db.GetConn()
+	assert.Len(t, db.conns, 0)
+}
+
+func TestDBGetConnReturnsErrorWhenDBIsClosed(t *testing.T) {
+	db := NewDB("root@tcp(127.0.0.1:3306)/test", 2)
+	db.Close()
+	_, err := db.GetConn()
+	assert.Equal(t, err, ErrClosedDB)
+}
+
 func TestParseDataSourceFull(t *testing.T) {
 	source := "root:123@tcp(127.0.0.1:3306)/test"
 	usr, pass, proto, addr, dbname := parseDataSource(source)
