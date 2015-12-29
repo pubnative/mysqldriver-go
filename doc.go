2b85bb822e4eb4da051e8fdb553226f91413290f
Kostiantyn Stepaniuk <kostya.stepanyuk@gmail.com>
2015-12-29 20:26:17 +0100

    Add notes about concurrency

--- doc.go
+++ doc.go
@@ -1,4 +1,21 @@
 /*
 Package mysqldriver is a driver for MySQL database
+
+Concurrency
+
+DB struct manages pool of connection to MySQL. Connection itself
+isn't thread-safe, so it should be obtained per every go-routine.
+
+ db := mysqldriver.NewDB("root@tcp(127.0.0.1:3306)/test", 10)
+ for i := 0; i < 10; i++ {
+ 	go func() {
+ 		conn, err := db.GetConn()
+ 		if err != nil {
+ 			// handle error
+ 		}
+ 		defer db.PutConn(conn) // return connection to the pool
+ 		// perform queries
+ 	}()
+ }
 */
 package mysqldriver
