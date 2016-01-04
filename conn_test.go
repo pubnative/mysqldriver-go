45409c6927d709cac42182431f2c1f076056276d
Kostiantyn Stepaniuk <kostya.stepanyuk@gmail.com>
2016-01-04 11:55:47 +0100

    Test connection establishing

---
+++ conn_test.go
@@ -0,0 +1,23 @@
+package mysqldriver
+
+import (
+	"github.com/pubnative/mysqlproto-go"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewConnSuccess(t *testing.T) {
+	_, err := NewConn("root", "", "tcp", "127.0.0.1:3306", "test")
+	assert.Nil(t, err)
+}
+
+func TestNewConnError(t *testing.T) {
+	_, err := NewConn("root", "", "tcp", "127.0.0.1:3306", "unknown")
+	assert.NotNil(t, err)
+	errPkt, ok := err.(mysqlproto.ERRPacket)
+	assert.True(t, ok)
+	assert.Equal(t, errPkt.ErrorCode, uint16(1049))
+	assert.Equal(t, errPkt.SQLState, "42000")
+	assert.Equal(t, errPkt.ErrorMessage, "Unknown database 'unknown'")
+}
